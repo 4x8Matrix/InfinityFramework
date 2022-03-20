9e5b69491cd1c1721a32126d7689e5e2674d1c46
AsynchronousMatrix <38085006+4x8Matrix@users.noreply.github.com>
2022-03-20 20:21:45 +0000

    Plugin Update & Table Update

--- source/LibaryManager/Table/init.lua
+++ source/LibaryManager/Table/init.lua
@@ -2,12 +2,77 @@
 local TableLibary = { Name = "Table" }
 
 -- // Table-Sub Libaries
-TableLibary.Utils = require(script.Utils)
 TableLibary.Dimentional = require(script.Dimentional)
 
 -- // Initialize
 for Index, Value in next, table do
     TableLibary[Index] = Value
+end
+
+function TableLibary.Clone(Source)
+    local Result = { }
+
+    for Index, Value in pairs(Source) do
+        local valueType = type(Value)
+
+        if valueType == "table" then
+            Result[Index] = self.Clone(Value)
+        else
+            Result[Index] = Value
+        end
+    end
+
+    return Result
+end
+
+function TableLibary.Transform(Source, Transform)
+    for Index, Value in pairs(Source) do
+        Source[Index] = Transform(Value)
+    end
+
+    return Source
+end
+
+function TableLibary.GetKeys(Source)
+    local Keys = { }
+
+    for Index in pairs(Source) do
+        Keys[#Keys + 1] = Index
+    end
+
+    return Keys
+end
+
+function TableLibary.GetValues(Source)
+    local Values = { }
+
+    for _, Value in pairs(Source) do
+        Values[#Values + 1] = Value
+    end
+
+    return Values
+end
+
+function TableLibary.Merge(Table0, Table1)
+    for Index, Value in pairs(Table1) do
+        Table0[Index] = Value
+    end
+
+    return Table0
+end
+
+function TableLibary.Fill(TblA, TblB)
+    for Index, Object in pairs(TblB) do
+        if TblA[Index] then
+            if type(TblA[Index]) == "table" then
+                self.Fill(TblA[Index], Object)
+            end
+        else
+            TblA[Index] = Object
+        end
+    end
+
+    return TblA
 end
 
 return TableLibary
